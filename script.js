abac1996c6fee8ce36efb2d2f6da3cbd7f6603b1
Renata Horvat <nemglan@gmail.com>
2025-09-10 12:12:16 +0200

    Remove old project files (index.html, script.js, .DS_Store)

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('kontakt-forma').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Hvala na poruci! Uskoro ćemo vam se javiti.');
-    this.reset();
-  });
-  
